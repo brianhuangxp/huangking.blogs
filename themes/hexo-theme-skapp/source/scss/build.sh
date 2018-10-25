abcf88a30280127ddd1e83f166d6eaa246b77e4f
brian.huang <brian.huang@ringcentral.com>
2018-10-25 19:32:04 +0800

    windows

---
+++ themes/hexo-theme-skapp/source/scss/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sass base/index.scss base/index.css
+sass views/page/index.scss views/page/index.css
+sass views/page/about.scss views/page/about.css
+sass views/page/archive.scss views/page/archive.css
+sass views/page/category.scss views/page/category.css
+sass views/page/error.scss views/page/error.css
+sass views/page/search.scss views/page/search.css
+sass views/page/post.scss views/page/post.css
+sass views/page/tag.scss views/page/tag.css
